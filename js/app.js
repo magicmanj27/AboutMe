0ee1078531107cd9978d813f052323298703a1cc
Jeremy Lee <www.jeremy.llee27@gmail.com>
2019-01-29 20:00:54 -0800

    Updating the the js files as they were not configured correctly

--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@
 
 var myFavoriteColors = ["black", "green", "purple"];
 
+console.log(myFavoriteColors);
+
 var guessMyFavoriteColor = prompt("What are my favorite color?");
 
 for (var i = 0; i < myFavoriteColors.length; i++) {
